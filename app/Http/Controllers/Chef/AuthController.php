8ecadc2535928c7d2ed40518f996238c46860877
hossam <harrazhossam11@gmail.com>
2024-09-18 23:56:21 +0300

    create trait and handle controller

--- app/Http/Controllers/Chef/AuthController.php
+++ app/Http/Controllers/Chef/AuthController.php
@@ -10,38 +10,47 @@ use Dotenv\Validator as DotenvValidator;
 use Illuminate\Contracts\Validation\Validator as ValidationValidator;
 use Validator;
 
+
+use App\Traits\ApiResponseTrait;
+
 class AuthController extends Controller
 {
+    use ApiResponseTrait ;
+
     public function register(Request $request){
-        $validator = Validator::make($request->all(), [
-            'name' => 'required|string|between:2,100',
-            'email' => 'required|string|email|max:100|unique:chefs',
-            'password' => 'required|string|min:6',
-        ]);
+        try{
+            $validator = Validator::make($request->all(), [
+                'name' => 'required|string|between:2,100',
+                'email' => 'required|string|email|max:100|unique:chefs',
+                'password' => 'required|string|min:6',
+            ]);
+        
+            if ($validator->fails()) {
+                return response()->json($validator->errors(), 400);
+            }
+        
+            $chef = Chef::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => bcrypt($request->password),
+            ]);
     
-        if ($validator->fails()) {
-            return response()->json($validator->errors(), 400);
+    
+    
+            $token = auth('chef')->attempt($request->only('email', 'password'));
+            $chef->token = $token ;
+    
+            if (!$token) {
+                return response()->json(['error' => 'Unauthorized'], 401);
+            }
+        
+            return $this->ApiResponse($chef , 'Chef successfully registered' , 201);
+        }catch(\Exception $e){
+            return response()->json([
+                'error' => 'Something went wrong',
+                'message' => $e->getMessage()], 500);
         }
-    
-        // إنشاء المستخدم
-        $User = Chef::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => bcrypt($request->password),
-        ]);
-
-
-
-        $token = auth('chef')->attempt($request->only('email', 'password'));
-
-        if (!$token) {
-            return response()->json(['error' => 'Unauthorized'], 401);
-        }
-    
-        return response()->json([
-            'message' => 'Admin successfully registered',
-            'token' => $token,
-        ], 201);
+      
 
     }
 }
--- app/Http/Controllers/User/AuthController.php
+++ app/Http/Controllers/User/AuthController.php
@@ -10,38 +10,46 @@ use Dotenv\Validator as DotenvValidator;
 use Illuminate\Contracts\Validation\Validator as ValidationValidator;
 use Validator;
 
+
+use App\Traits\ApiResponseTrait;
+
 class AuthController extends Controller
 {
-    public function register(Request $request){
-        $validator = Validator::make($request->all(), [
-            'name' => 'required|string|between:2,100',
-            'email' => 'required|string|email|max:100|unique:users',
-            'password' => 'required|string|min:6',
-        ]);
+    use ApiResponseTrait ;
+    try{
+        public function register(Request $request){
+            $validator = Validator::make($request->all(), [
+                'name' => 'required|string|between:2,100',
+                'email' => 'required|string|email|max:100|unique:users',
+                'password' => 'required|string|min:6',
+            ]);
+        
+            if ($validator->fails()) {
+                return response()->json($validator->errors(), 400);
+            }
+        
+            // إنشاء المستخدم
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => bcrypt($request->password),
+            ]);
     
-        if ($validator->fails()) {
-            return response()->json($validator->errors(), 400);
+            $token = auth('api')->attempt($request->only('email', 'password'));
+            $user->token = $token ;
+    
+            if (!$token) {
+                return response()->json(['error' => 'Unauthorized'], 401);
+            }
+        
+            return $this->ApiResponse($user , 'Chef successfully registered' , 201);
+    
         }
-    
-        // إنشاء المستخدم
-        $User = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => bcrypt($request->password),
-        ]);
-
-
-
-        $token = auth('api')->attempt($request->only('email', 'password'));
-
-        if (!$token) {
-            return response()->json(['error' => 'Unauthorized'], 401);
-        }
-    
-        return response()->json([
-            'message' => 'Admin successfully registered',
-            'token' => $token,
-        ], 201);
-
     }
+    catch(\Exception $e){
+        return response()->json([
+        'error' => 'Something went wrong',
+        'message' => $e->getMessage()], 500);
+    }
+    
 }
