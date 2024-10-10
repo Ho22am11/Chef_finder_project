74e893970a0a1118fa5e32061d2ff3ee18d0aa1d
hossam <harrazhossam11@gmail.com>
2024-10-10 21:32:38 +0300

    test one

---
+++ app/Http/Controllers/MessageController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Events\MessageSent;
+use App\Models\Message;
+
+class MessageController extends Controller
+{
+    public function send(Request $request)
+    {
+            $message = Message::create([
+                'sender_id' => 1 ,
+                'sender_type' => $request->sender_type ,
+                'receiver_id' => $request->receiver_id,
+                'receiver_type' => $request->receiver_type,
+                'message' => $request->message,
+         ]);
+
+         broadcast(new MessageSent($message))->toOthers();
+
+         return response()->json(['status' => 'Message Sent!']);
+    }
+
+}
--- routes/channels.php
+++ routes/channels.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Broadcast;
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
+
+
+Broadcast::channel('chatt.{receiverId}', function ($user, $receiverId) {
+  return $user->id == $receiverId; // تحقق أن المستخدم الحالي هو المستلم الصحيح
+});
